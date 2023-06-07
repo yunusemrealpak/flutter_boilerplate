9956be89dad8237ff9272ac2fc93de6fd2865fe8
Yunus Emre Alpak <yunusemrealpak@gmail.com>
2023-06-07 20:29:44 +0300

    fixed bugg with update

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -11,35 +11,37 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:busenet/busenet.dart' as _i9;
 import 'package:flutter_boilerplate/application/app/app_cubit.dart' as _i3;
-import 'package:flutter_boilerplate/application/auth/auth_cubit.dart' as _i19;
+import 'package:flutter_boilerplate/application/auth/auth_cubit.dart' as _i20;
 import 'package:flutter_boilerplate/application/sample/sample_cubit.dart'
-    as _i12;
+    as _i11;
 import 'package:flutter_boilerplate/application/services/deeplink/deeplink_cubit.dart'
-    as _i20;
+    as _i21;
 import 'package:flutter_boilerplate/application/session/session_cubit.dart'
     as _i13;
 import 'package:flutter_boilerplate/application/splash/splash_cubit.dart'
     as _i14;
 import 'package:flutter_boilerplate/core/storage/hive/i_cache_service.dart'
     as _i4;
-import 'package:flutter_boilerplate/domain/auth/i_auth_repository.dart' as _i15;
-import 'package:flutter_boilerplate/domain/cache/model/config.dart' as _i7;
-import 'package:flutter_boilerplate/domain/cache/model/token_model.dart' as _i5;
+import 'package:flutter_boilerplate/domain/auth/i_auth_repository.dart' as _i16;
+import 'package:flutter_boilerplate/domain/cache/model/config.dart' as _i5;
+import 'package:flutter_boilerplate/domain/cache/model/token_model.dart' as _i7;
 import 'package:flutter_boilerplate/domain/deeplink/i_deeplink_repository.dart'
-    as _i17;
-import 'package:flutter_boilerplate/domain/sample/i_sample_repository.dart'
-    as _i10;
-import 'package:flutter_boilerplate/infrastructure/auth/auth_repository.dart'
-    as _i16;
-import 'package:flutter_boilerplate/infrastructure/cache/config_cache_service.dart'
-    as _i8;
-import 'package:flutter_boilerplate/infrastructure/cache/token_cache_service.dart'
-    as _i6;
-import 'package:flutter_boilerplate/infrastructure/deeplink/deeplink_repository.dart'
     as _i18;
-import 'package:flutter_boilerplate/infrastructure/sample/sample_repository.dart'
-    as _i11;
-import 'package:flutter_boilerplate/injection_module.dart' as _i21;
+import 'package:flutter_boilerplate/domain/network/app_network_manager.dart'
+    as _i15;
+import 'package:flutter_boilerplate/domain/network/model/response_model.dart'
+    as _i10;
+import 'package:flutter_boilerplate/domain/sample/i_sample_repository.dart'
+    as _i12;
+import 'package:flutter_boilerplate/infrastructure/auth/auth_repository.dart'
+    as _i17;
+import 'package:flutter_boilerplate/infrastructure/cache/config_cache_service.dart'
+    as _i6;
+import 'package:flutter_boilerplate/infrastructure/cache/token_cache_service.dart'
+    as _i8;
+import 'package:flutter_boilerplate/infrastructure/deeplink/deeplink_repository.dart'
+    as _i19;
+import 'package:flutter_boilerplate/injection_module.dart' as _i22;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -56,36 +58,36 @@ extension GetItInjectableX on _i1.GetIt {
     );
     final injectionModule = _$InjectionModule();
     gh.lazySingleton<_i3.AppCubit>(() => _i3.AppCubit());
-    gh.lazySingleton<_i4.ICacheManager<_i5.TokenModel>>(
-      () => _i6.TokenCacheService(),
-      instanceName: 'token_service',
-    );
-    gh.lazySingleton<_i4.ICacheManager<_i7.Config>>(
-      () => _i8.ConfigCacheService(),
+    gh.lazySingleton<_i4.ICacheManager<_i5.Config>>(
+      () => _i6.ConfigCacheService(),
       instanceName: 'config_service',
     );
-    gh.singleton<_i9.INetworkManager<_i9.EmptyResponseModel>>(
+    gh.lazySingleton<_i4.ICacheManager<_i7.TokenModel>>(
+      () => _i8.TokenCacheService(),
+      instanceName: 'token_service',
+    );
+    gh.singleton<_i9.INetworkManager<_i10.ResponseModel>>(
         injectionModule.manager);
-    gh.lazySingleton<_i10.ISampleRepository>(() => _i11.SampleRepository(
-        gh<_i9.INetworkManager<_i9.EmptyResponseModel>>()));
-    gh.factory<_i12.SampleCubit>(
-        () => _i12.SampleCubit(gh<_i10.ISampleRepository>()));
+    gh.factory<_i11.SampleCubit>(
+        () => _i11.SampleCubit(gh<_i12.ISampleRepository>()));
     gh.factory<_i13.SessionCubit>(() => _i13.SessionCubit());
     gh.factory<_i14.SplashCubit>(() => _i14.SplashCubit());
-    gh.lazySingleton<_i15.IAuthRepository>(() => _i16.AuthRepository(
-          gh<_i9.INetworkManager<_i9.EmptyResponseModel>>(),
-          gh<_i4.ICacheManager<_i5.TokenModel>>(instanceName: 'config_service'),
+    gh.singleton<_i15.AppNetworkManager>(
+        _i15.AppNetworkManager(gh<_i9.INetworkManager<_i10.ResponseModel>>()));
+    gh.lazySingleton<_i16.IAuthRepository>(() => _i17.AuthRepository(
+          gh<_i15.AppNetworkManager>(),
+          gh<_i4.ICacheManager<_i7.TokenModel>>(instanceName: 'config_service'),
         ));
-    gh.lazySingleton<_i17.IDeeplinkRepository>(() => _i18.DeeplinkRepository(
-        gh<_i9.INetworkManager<_i9.EmptyResponseModel>>()));
-    gh.factory<_i19.AuthCubit>(
-        () => _i19.AuthCubit(gh<_i15.IAuthRepository>()));
-    gh.lazySingleton<_i20.DeeplinkCubit>(() => _i20.DeeplinkCubit(
-          gh<_i4.ICacheManager<_i7.Config>>(instanceName: 'config_service'),
-          gh<_i17.IDeeplinkRepository>(),
+    gh.lazySingleton<_i18.IDeeplinkRepository>(
+        () => _i19.DeeplinkRepository(gh<_i15.AppNetworkManager>()));
+    gh.factory<_i20.AuthCubit>(
+        () => _i20.AuthCubit(gh<_i16.IAuthRepository>()));
+    gh.lazySingleton<_i21.DeeplinkCubit>(() => _i21.DeeplinkCubit(
+          gh<_i4.ICacheManager<_i5.Config>>(instanceName: 'config_service'),
+          gh<_i18.IDeeplinkRepository>(),
         ));
     return this;
   }
 }
 
-class _$InjectionModule extends _i21.InjectionModule {}
+class _$InjectionModule extends _i22.InjectionModule {}
--- lib/presentation/_route/router.gr.dart
+++ lib/presentation/_route/router.gr.dart
@@ -10,26 +10,21 @@
 part of 'router.dart';
 
 abstract class _$AppRouter extends RootStackRouter {
-  _$AppRouter([GlobalKey<NavigatorState>? navigatorKey]) : super(navigatorKey);
+  // ignore: unused_element
+  _$AppRouter({super.navigatorKey});
 
   @override
   final Map<String, PageFactory> pagesMap = {
-    SplashRoute.name: (routeData) {
+    AppRoute.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const SplashView(),
+        child: const AppView(),
       );
     },
     HomeRoute.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const HomeView(),
-      );
-    },
-    AppRoute.name: (routeData) {
-      return AutoRoutePage<dynamic>(
-        routeData: routeData,
-        child: const AppView(),
       );
     },
     ProfileRoute.name: (routeData) {
@@ -44,16 +39,16 @@ abstract class _$AppRouter extends RootStackRouter {
         child: const ForgotPasswordView(),
       );
     },
-    RegisterRoute.name: (routeData) {
-      return AutoRoutePage<dynamic>(
-        routeData: routeData,
-        child: const RegisterView(),
-      );
-    },
     LoginRoute.name: (routeData) {
       return AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const LoginView(),
+      );
+    },
+    RegisterRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const RegisterView(),
       );
     },
     OnboardingRoute.name: (routeData) {
@@ -62,19 +57,25 @@ abstract class _$AppRouter extends RootStackRouter {
         child: const OnboardingView(),
       );
     },
+    SplashRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const SplashView(),
+      );
+    },
   };
 }
 
 /// generated route for
-/// [SplashView]
-class SplashRoute extends PageRouteInfo<void> {
-  const SplashRoute({List<PageRouteInfo>? children})
+/// [AppView]
+class AppRoute extends PageRouteInfo<void> {
+  const AppRoute({List<PageRouteInfo>? children})
       : super(
-          SplashRoute.name,
+          AppRoute.name,
           initialChildren: children,
         );
 
-  static const String name = 'SplashRoute';
+  static const String name = 'AppRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
 }
@@ -89,20 +90,6 @@ class HomeRoute extends PageRouteInfo<void> {
         );
 
   static const String name = 'HomeRoute';
-
-  static const PageInfo<void> page = PageInfo<void>(name);
-}
-
-/// generated route for
-/// [AppView]
-class AppRoute extends PageRouteInfo<void> {
-  const AppRoute({List<PageRouteInfo>? children})
-      : super(
-          AppRoute.name,
-          initialChildren: children,
-        );
-
-  static const String name = 'AppRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
 }
@@ -136,20 +123,6 @@ class ForgotPasswordRoute extends PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [RegisterView]
-class RegisterRoute extends PageRouteInfo<void> {
-  const RegisterRoute({List<PageRouteInfo>? children})
-      : super(
-          RegisterRoute.name,
-          initialChildren: children,
-        );
-
-  static const String name = 'RegisterRoute';
-
-  static const PageInfo<void> page = PageInfo<void>(name);
-}
-
-/// generated route for
 /// [LoginView]
 class LoginRoute extends PageRouteInfo<void> {
   const LoginRoute({List<PageRouteInfo>? children})
@@ -164,6 +137,20 @@ class LoginRoute extends PageRouteInfo<void> {
 }
 
 /// generated route for
+/// [RegisterView]
+class RegisterRoute extends PageRouteInfo<void> {
+  const RegisterRoute({List<PageRouteInfo>? children})
+      : super(
+          RegisterRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'RegisterRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
 /// [OnboardingView]
 class OnboardingRoute extends PageRouteInfo<void> {
   const OnboardingRoute({List<PageRouteInfo>? children})
@@ -173,6 +160,20 @@ class OnboardingRoute extends PageRouteInfo<void> {
         );
 
   static const String name = 'OnboardingRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [SplashView]
+class SplashRoute extends PageRouteInfo<void> {
+  const SplashRoute({List<PageRouteInfo>? children})
+      : super(
+          SplashRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'SplashRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
 }
